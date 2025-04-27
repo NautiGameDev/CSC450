0460aae16c8d7102ff5f8076603bdd95eeb37e7c
Nauti <chrisrussell167@gmail.com>
2025-04-26 21:36:56 -0600

    Add files via upload

--- Module 2/Mod2CTA.cpp
+++ Module 2/Mod2CTA.cpp
@@ -4,31 +4,71 @@ Module 2 Critical Thinking Assignment
 CSC450: Programming III
 April 26, 2025
 */
+
 #include <iostream>
 #include <cstring>
+#include <limits>
 
 using namespace std;
 
-int main()
+char* GetWord(int wordSize)
 {
-    cout << "\nConcatenate Application" << endl;
+    bool isInputValid = false;
 
-    char firstWord[25];
-    char secondWord[25];
+    int adjustedWordSize = wordSize + 1; //Add 1 to wordsize in order to ensure space for null terminator
+    char* word = new char[adjustedWordSize];
 
-    cout << "\nEnter your first word." << endl;
-    cout << " >> ";
-    cin.getline(firstWord, sizeof(firstWord));
+    //While loop ensures user input is within the scope of cstring size. 
+    //If word is too large, the user is prompted again for another word, else return the word.
+    while(!isInputValid)
+    {
+        cout << "Input >> ";
+        cin.getline(word, adjustedWordSize);
 
-    cout << "\nEnter your second word." << endl;
-    cout << " >> ";
-    cin.getline(secondWord, sizeof(secondWord));
+        if (cin.fail())
+        {
+            cout << "\nWord is too large. Please enter a word less than " << (wordSize) << " characters." << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        }
+        else
+        {
+            isInputValid = true;
+            return word;
+        }
+    }
+}
+
+void ConcatenateWordsBySize(int wordSize)
+{
+    //Sets cstring to wordSize + 1 to ensure room for null terminator
+    char firstWord[wordSize + 1];
+    char secondWord[wordSize + 1];
+
+    cout << "\nEnter a word less than " << wordSize << " characters." << endl;
+    char* tempFirst = GetWord(wordSize);
+    strcpy(firstWord, tempFirst);
+    
+    cout << "\nEnter another word less than " << wordSize << " characters." << endl;
+    char* tempSecond = GetWord(wordSize);
+    strcpy(secondWord, tempSecond);
 
     strcat(firstWord, secondWord);
-    cout << "Concatenated words: " << firstWord << endl;
+    cout << "\nConcatenated words: " << firstWord << endl;
+}
+
+int main()
+{
+    cout << " >> ";
+    cout << "\nConcatenate Application" << endl;
+
+    //Call function to get two inputs from user for different sizes.
+    ConcatenateWordsBySize(4);
+    ConcatenateWordsBySize(7);
+    ConcatenateWordsBySize(11);
 
     cout << "\nPress enter key to close the application" << endl;
-    cin.get();
 
+    cin.get();
     return 1;
 }
